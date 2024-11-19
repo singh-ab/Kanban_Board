d61b6edeb7ad9201e1cead993e3e669776c1a40d
Abhay Singh <singhxabhay@gmail.com>
2024-11-20 00:55:58 +0530

    "added utility for status icon"

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,6 +2,8 @@ import "./card.css";
 import ThreeDotMenu from "../assets/icons_FEtask/Img - High Priority.svg";
 import { getStatus } from "../utils/status";
 import { Dot, HighPriority, InProgress, LowPriority } from "../utils/Svgs";
+import { getPriorityIcon } from "../utils/priority";
+
 const Card = ({ data, userName, status }) => {
   console.log("data", data);
 
@@ -15,19 +17,19 @@ const Card = ({ data, userName, status }) => {
             <h3 className="card-cam">{data?.id}</h3>
           </div>
           <div className="card-top">
-            <p className="card-description">
-              {status && (
-                <img src={getStatus()} alt="Menu icon" className="menu-icon" />
-              )}{" "}
-              {" Status"}
-              {data?.title}
-            </p>
+          <p className="card-description">
+            {data?.status && (
+              <img src={getStatus(data.status)} alt="Menu icon" className="menu-icon" />
+            )}
+            {" Status"}
+            {data?.title}
+          </p>
           </div>
           <div style={{ display: "flex", gap: "0.5rem", fontSize: "0.01rem" }}>
             <div className="card-network-icon">
               <img
-                src={HighPriority}
-                alt="Menu icon"
+                src={getPriorityIcon(data?.priority)}
+                alt="Priority icon"
                 style={{ width: "0.7rem" }}
               />
             </div>
